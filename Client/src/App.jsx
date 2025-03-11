1899fce3d9922143ccb0ec767f9851e6dd4f2cbd
gopu-005 <gopikrishnashenoy.s@gmail.com>
2025-03-11 14:12:09 +0530

    change

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import LandingPage from './Pages/LandingPage'
 import JoinCommunity from './Pages/JoinCommunity'
-import Project_submission from './Pages/Project_submission'
+// import Project_submission from './Pages/Project_submission'
 const App = () => {
   return (
     <>
@@ -10,7 +10,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/join-community" element={<JoinCommunity />} />
-          <Route path="/project-submission" element={<Project_submission />} />
+          {/* <Route path="/project-submission" element={<Project_submission />} /> */}
         </Routes>
       </Router>
     </>
